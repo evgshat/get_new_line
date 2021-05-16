981d78622d36616f0f405087a633360f46f58c2c
Шаталюк Евгения Олеговна <EOShatalyuk@sberbank.ru>
2021-05-16 22:00:05 +0300

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -1,9 +1,27 @@
-# include "../get_next_line.h"
+#include "get_next_line.h"
 
 int	get_next_line(int fd, char **line)
 {
-	while (!EOF)
-	{
+	int		byte_was_read;
+	char	buf[5];
 
+	*line = "\0";
+	while ((byte_was_read = read(fd, buf, 5)))
+	{
+		if (ft_strchr(buf, '\n'))
+			break ;
+		buf[byte_was_read] = '\0';
+		*line = ft_strjoin(*line, buf);
 	}
+	return (0);
+}
+
+int	main(void)
+{
+	char	*line;
+	int		fd;
+
+	fd = open ("text.txt", O_RDONLY);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,8 +1,14 @@
-# ifndef GET_NEW_LINE
-# define GET_NEW_LINE
+#ifndef GET_NEW_LINE_H
+# define GET_NEW_LINE_H
 
 # include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
 
-int	get_next_line(int fd, char **line);
+int			get_next_line(int fd, char **line);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strchr (const char *s, int c);
 
-# endif
+#endif
